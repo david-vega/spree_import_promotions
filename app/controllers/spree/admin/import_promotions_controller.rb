562cc9dd0caed0aaeaf1d90da0f8f24827853f87
David Vega <zetacu@gmail.com>
2014-05-06 11:34:24 -0500

    add controller actions and import view

--- app/controllers/spree/admin/import_promotions_controller.rb
+++ app/controllers/spree/admin/import_promotions_controller.rb
@@ -1,3 +1,19 @@
 class Spree::Admin::ImportPromotionsController < Spree::Admin::BaseController
+  def new
 
+  end
+
+  def create
+
+  end
+
+  private
+
+  def valid_csv_extension?(file)
+    file.original_filename[-3..-1].downcase == 'csv'
+  end
+
+  def valid_encoding?(file)
+    file.read.force_encoding('iso8859-1').encode('utf-8').valid_encoding?
+  end
 end
--- app/overrides/add_import_promotions_controller.rb
+++ app/overrides/add_import_promotions_controller.rb
@@ -1,5 +1,7 @@
 Deface::Override.new(virtual_path: 'spree/admin/promotions/index',
                      name: 'add_import_promotion_button',
                      insert_before: 'li',
-                     text: '<li style="margin-right: 5px"><a href="/admin/import_promotions/new" class="button icon-plus" icon="icon-plus">Importar promociones</a></li>'
+                     text: '<li style="margin-right: 5px">
+                            <a href="/shop/admin/import_promotions/new" class="button icon-plus" icon="icon-plus">
+                            Import Promotions</a></li>'
                     )
