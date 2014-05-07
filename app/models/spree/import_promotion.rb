45fa017e6dba628674fb5020b728ef8978a2d9d3
David Vega <zetacu@gmail.com>
2014-05-07 12:04:12 -0500

    set a flat rate action to the promotion

--- app/models/spree/import_promotion.rb
+++ app/models/spree/import_promotion.rb
@@ -1,20 +1,35 @@
 require 'csv'
 
 class Spree::ImportPromotion
+
   def self.import_csv(file)
     CSV.foreach(file.path, headers: true) do |row|
-      promotion = generate_promotion(row)
+      binding.pry
+      data = row.to_hash
+      promotion = find_or_create_promotion(data)
+      action(data['AMOUNT'], promotion)
     end
   end
 
-  def self.generate_promotion(row)
-    promotion = Spree::Promotion.new
+  def self.find_or_create_promotion(row)
+    promotion = Spree::Promotion.find_by_code(row['CODE'])
 
-    row.to_hash.each do |key, value|
-      model_key = key.to_s.gsub(' ', '_').downcase
-      promotion[model_key] = value if promotion.has_attribute?(model_key)
+    unless promotion
+      promotion = Spree::Promotion.new
+
+      row.each do |key, value|
+        model_key = key.to_s.gsub(' ', '_').downcase
+        promotion[model_key] = value if promotion.has_attribute?(model_key)
+      end
+
+      promotion.save!
     end
+    promotion
+  end
 
-    promotion.save!
+  def self.action(amount, promotion)
+    calculator = Spree::Calculator::FlatRate.create(preferred_amount: amount)
+    promotion = Spree::Promotion::Actions::CreateAdjustment.create(promotion: promotion,
+                                                            calculator: calculator)
   end
 end
