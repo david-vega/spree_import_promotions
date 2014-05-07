10cc1bb71b26bcdc16d56dac38fbac2f121925c9
David Vega <zetacu@gmail.com>
2014-05-07 12:14:05 -0500

    refactor import logic

--- app/models/spree/import_promotion.rb
+++ app/models/spree/import_promotion.rb
@@ -4,32 +4,41 @@ class Spree::ImportPromotion
 
   def self.import_csv(file)
     CSV.foreach(file.path, headers: true) do |row|
-      binding.pry
       data = row.to_hash
+
       promotion = find_or_create_promotion(data)
-      action(data['AMOUNT'], promotion)
+      promotion.actions.destroy_all
+      promotion.actions << action(data['AMOUNT'])
+
+      promotion
     end
   end
+
+  private
 
   def self.find_or_create_promotion(row)
     promotion = Spree::Promotion.find_by_code(row['CODE'])
 
     unless promotion
-      promotion = Spree::Promotion.new
-
-      row.each do |key, value|
-        model_key = key.to_s.gsub(' ', '_').downcase
-        promotion[model_key] = value if promotion.has_attribute?(model_key)
-      end
-
-      promotion.save!
+      promotion = create_promotion(row)
     end
+
     promotion
   end
 
-  def self.action(amount, promotion)
-    calculator = Spree::Calculator::FlatRate.create(preferred_amount: amount)
-    promotion = Spree::Promotion::Actions::CreateAdjustment.create(promotion: promotion,
-                                                            calculator: calculator)
+  def self.create_promotion(row)
+    promotion = Spree::Promotion.new
+
+    row.each do |key, value|
+      model_key = key.to_s.gsub(' ', '_').downcase
+      promotion[model_key] = value if promotion.has_attribute?(model_key)
+    end
+    promotion.save!
+    promotion
+  end
+
+  def self.action(amount)
+    calculator = Spree::Calculator::FlatRate.new(preferred_amount: amount)
+    promotion = Spree::Promotion::Actions::CreateAdjustment.new(calculator: calculator)
   end
 end
